8c548189535b33b91fe8068d0cae194e88c74b4c
kotchaniphapk <kotchanipha.pk@gmail.com>
2026-01-28 21:45:28 +0700

    fixed error

--- public/ThaiMe_files/a89ba_next_dist_client_components_builtin_global-error_f94d3993.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/a89ba_next_dist_ec3a4297._.js"
-],
-    source: "dynamic"
-});
--- public/ThaiMe_files/twodayweb-work_costomer_thaiMe_src_app_layout_tsx_f94d3993._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__980eadfa._.css",
-  "static/chunks/a89ba_ca79950f._.js",
-  "static/chunks/twodayweb-work_costomer_thaiMe_src_267d3ef5._.js"
-],
-    source: "dynamic"
-});
